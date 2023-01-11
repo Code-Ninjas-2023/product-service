bf39e02261a07be539cc041fe347c1d624ce53ab
EdiTeare <edomteare96@gmail.com>
2023-01-11 13:30:48 -0800

    added create product

--- src/main/java/com/codeninjas/productservice/service/ProductService.java
+++ src/main/java/com/codeninjas/productservice/service/ProductService.java
@@ -1,4 +1,8 @@
 package com.codeninjas.productservice.service;
 
+import com.codeninjas.productservice.domain.Product;
+
 public interface ProductService {
+
+    Product addProduct (Product product);
 }
--- src/main/java/com/codeninjas/productservice/service/ProductServiceImpl.java
+++ src/main/java/com/codeninjas/productservice/service/ProductServiceImpl.java
@@ -1,10 +1,20 @@
 package com.codeninjas.productservice.service;
 
+import com.codeninjas.productservice.domain.Product;
+import com.codeninjas.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
