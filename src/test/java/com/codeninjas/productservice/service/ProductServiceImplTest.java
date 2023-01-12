e1537c7a971adc0019327fbfb6444375ca6e1f90
Mahlet Seyoum <102604633+MahletSeyoum@users.noreply.github.com>
2023-01-12 13:35:26 -0800

    unit test update and delete methods

---
+++ src/test/java/com/codeninjas/productservice/service/ProductServiceImplTest.java
@@ -0,0 +1,61 @@
+package com.codeninjas.productservice.service;
+
+import com.codeninjas.productservice.domain.Product;
+import com.codeninjas.productservice.repository.ProductRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    private Product product;
+
+    @Before
+    public void setUp() {
+        product = new Product();
+        product.setId("1123");
+        product.setName("laptop");
+        product.setDescription("macbook pro");
+        product.setPrice(BigDecimal.valueOf(999.00));
+    }
+
+    @Test
+    public void updateProduct() {
+        Product newProduct = new Product();
+        Mockito.when(productRepository.findById(any())).thenReturn(Optional.ofNullable(product));
+        newProduct.setId("1123");
+        newProduct.setName("laptop");
+        newProduct.setDescription("macbook pro");
+        newProduct.setPrice(BigDecimal.valueOf(1200.00));
+        Product response = productService.updateProduct("1123",newProduct);
+        assertEquals(response, newProduct);
+    }
+
+    @Test
+    public void deleteProduct() {
+        String productId = "1123";
+        willDoNothing().given(productRepository).deleteById(productId);
+        productService.deleteProduct(productId);
+        verify(productRepository, times(1)).deleteById(productId);
+    }
+}
