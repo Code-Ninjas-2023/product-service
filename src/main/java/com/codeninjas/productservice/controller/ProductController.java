b2d3e7e77df700985fdbd5cd472fc130bf5559c9
EdiTeare <edomteare96@gmail.com>
2023-01-12 14:40:15 -0800

    implemented delete and update Api

--- src/main/java/com/codeninjas/productservice/controller/ProductController.java
+++ src/main/java/com/codeninjas/productservice/controller/ProductController.java
@@ -1,12 +1,31 @@
 package com.codeninjas.productservice.controller;
 
+import com.codeninjas.productservice.domain.Product;
+import com.codeninjas.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
+
+private final ProductService productService;
+
+    @PutMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> updateById(@PathVariable String id, @RequestBody Product product) {
+        productService.updateProduct(id ,product);
+        return new ResponseEntity<String>("Updated",HttpStatus.OK);
+    }
+    @DeleteMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> deleteById(@PathVariable String id) {
+        productService.deleteProduct(id);
+        return new  ResponseEntity<String>("Deleted", HttpStatus.OK);
+
+    }
 
 }
