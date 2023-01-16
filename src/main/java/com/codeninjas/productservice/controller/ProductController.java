89d62adc581656dfc755486b455d0ba3a12a1201
Mahlet Seyoum <102604633+MahletSeyoum@users.noreply.github.com>
2023-01-15 17:27:32 -0800

    implemented apis for product service

--- src/main/java/com/codeninjas/productservice/controller/ProductController.java
+++ src/main/java/com/codeninjas/productservice/controller/ProductController.java
@@ -1,12 +1,33 @@
 package com.codeninjas.productservice.controller;
 
+import com.codeninjas.productservice.domain.Product;
+import com.codeninjas.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("{id}")
+    public Product getProductsById(@PathVariable String id){
+        return productService.getProductById(id);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.addProduct(product);
+    }
 }
