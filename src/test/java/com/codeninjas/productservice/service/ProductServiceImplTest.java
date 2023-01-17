3becd9120bb247ea20dc82dba0adfb8b2d182568
Mahlet Seyoum <102604633+MahletSeyoum@users.noreply.github.com>
2023-01-16 20:00:50 -0800

    unittest on service methods

--- src/test/java/com/codeninjas/productservice/service/ProductServiceImplTest.java
+++ src/test/java/com/codeninjas/productservice/service/ProductServiceImplTest.java
@@ -11,13 +11,14 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductServiceImplTest {
@@ -58,4 +59,26 @@ public class ProductServiceImplTest {
         productService.deleteProduct(productId);
         verify(productRepository, times(1)).deleteById(productId);
     }
+
+    @Test
+    public void addProduct() {
+        when(productRepository.save(any())).thenReturn(product);
+        productService.addProduct(product);
+        verify(productRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void getAllProducts() {
+        List<Product> products = new ArrayList<>();
+        when(productRepository.findAll()).thenReturn(products);
+        productService.getAllProducts();
+        verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void getProductById() {
+        String productId = "1123";
+        when(productRepository.findById(anyString())).thenReturn(Optional.ofNullable(product));
+        productService.getProductById(productId);
+        verify(productRepository, times(1)).findById(productId);    }
 }
