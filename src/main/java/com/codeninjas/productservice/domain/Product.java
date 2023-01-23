dceef477cd6cc8a39310b498b3c99a97cbccadaf
EdiTeare <edomteare96@gmail.com>
2023-01-23 15:23:26 -0800

    validation

--- src/main/java/com/codeninjas/productservice/domain/Product.java
+++ src/main/java/com/codeninjas/productservice/domain/Product.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 @Document(value = "product")
@@ -18,6 +21,10 @@ public class Product {
     @Id
     @Generated
     private String id;
+
+    // @Size(min=2, message = "Name should have at least 2 characters")
+    @NotNull(message = "Name is a required field")
+    @NotBlank
     private String name;
     private String description;
     private BigDecimal price;
